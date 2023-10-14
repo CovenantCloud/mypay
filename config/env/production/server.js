6c85e29e9b53c5a11fd3edfd7d24ef90ada076e7
Mathieu Boa <boa.matthieu@canafran.com>
2023-10-14 19:34:27 +0000

    v3

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1340),
-    url: env('HOST', 'https://nifty-pascal.74-208-60-233.plesk.page'),
+    url: env('RENDER_EXTERNAL_URL', ''),
   });
   
